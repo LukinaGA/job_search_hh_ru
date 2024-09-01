756283d59169c85c4c3f3f353804540168f3be71
Galina Lukina <lukina.galina11@gmail.com>
2024-09-01 15:55:15 +0400

    add method __api_connect

--- src/head_hunter_api.py
+++ src/head_hunter_api.py
@@ -6,7 +6,13 @@ from abc import ABC, abstractmethod
 class Parser(ABC):
 
     @abstractmethod
+    def __api_connect(self):
+        """Подключение к API"""
+        pass
+
+    @abstractmethod
     def load_vacancies(self, keyword):
+        """Получает вакансии по ключевому слову"""
         pass
 
 
@@ -14,15 +20,30 @@ class HeadHunterAPI(Parser):
     """Класс для работы с API HeadHunter"""
 
     def __init__(self):
-        self.url = 'https://api.hh.ru/vacancies'
-        self.headers = {'User-Agent': 'HH-User-Agent'}
-        self.params = {'text': '', 'page': 0, 'per_page': 100}
+        self.__url = 'https://api.hh.ru/vacancies'
+        self.__headers = {'User-Agent': 'HH-User-Agent'}
+        self.__params = {'text': '', 'page': 0, 'per_page': 100}
         self.vacancies = []
 
-    def load_vacancies(self, keyword):
-        self.params['text'] = keyword
-        while self.params.get('page') != 20:
-            response = requests.get(self.url, headers=self.headers, params=self.params)
-            vacancies = response.json()['items']
-            self.vacancies.extend(vacancies)
-            self.params['page'] += 1
+    def _Parser__api_connect(self):
+        """Подключение к API hh.ru"""
+        response = requests.get(self.__url, headers=self.__headers, params=self.__params)
+        if response.status_code == 200:
+            return response
+
+        print("Ошибка получения данных")
+
+    def load_vacancies(self, keyword: str) -> list:
+        """Получение вакансий по ключевому слову"""
+        self.__params['text'] = keyword
+        while self.__params.get('page') != 20:
+            response = self._Parser__api_connect()
+            if response:
+                vacancies = response.json()['items']
+                self.vacancies.extend(vacancies)
+                self.__params['page'] += 1
+            else:
+                self.vacancies = []
+                break
+
+        return self.vacancies
--- src/vacancy.py
+++ src/vacancy.py
@@ -1,5 +1,6 @@
 class Vacancy:
     """Класс для представления вакансий"""
+    __slots__ = ("name", "url", "description", "salary")
 
     def __init__(self, name, url, description, salary=0):
         self.name = name
