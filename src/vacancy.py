e7f386abe471efef1537333d934acdd70553e884
Galina Lukina <lukina.galina11@gmail.com>
2024-09-05 13:25:32 +0400

    add comparison methods for Vacancy

--- src/vacancy.py
+++ src/vacancy.py
@@ -6,12 +6,18 @@ class Vacancy:
         self.name = name
         self.url = url
         self.description = description
-        self.salary = self.__salary_validation(salary)
+        try:
+            self.salary = self.__salary_validation(salary)
+        except TypeError as te:
+            self.salary = 0
+            print(f"{te}, будет указано значение по умолчанию (0)")
 
     @staticmethod
-    def __salary_validation(salary):
+    def __salary_validation(salary: float):
         if salary:
-            return salary
+            if type(salary) == float:
+                return salary
+            raise TypeError("Зарплата должна быть числом")
         return 0
 
     @classmethod
@@ -21,6 +27,25 @@ class Vacancy:
 
     def __str__(self):
         return f"{self.name} (ЗП {self.salary} руб). {self.description}. Ссылка на вакансию: {self.url}"
+
+    @classmethod
+    def __verify_data(cls, other):
+        if not isinstance(other, (float, Vacancy)):
+            raise TypeError
+
+        return other if isinstance(other, float) else other.salary
+
+    def __eq__(self, other):
+        sal = self.__verify_data(other)
+        return self.salary == sal
+
+    def __lt__(self, other):
+        sal = self.__verify_data(other)
+        return self.salary < sal
+
+    def __lg__(self, other):
+        sal = self.__verify_data(other)
+        return self.salary <= sal
 
     def cast_to_object_list(self, vacancies: list[dict]) -> object:
         """Считывает данные из JSON-файла и на их основе создает объекты классов"""
@@ -41,4 +66,3 @@ class Vacancy:
             vacancies_list.append(vac)
 
         return vacancies_list
-    
