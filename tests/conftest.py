7c96ee34c2d8402b35eb8b9eb2d5b01e40fdfacc
Galina Lukina <lukina.galina11@gmail.com>
2024-09-09 18:08:51 +0400

    add tests vacancy.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,18 +6,18 @@ from src.vacancy import Vacancy
 def vacancies_dict():
     vacs = [
         {
-            "name": "Middle QA Engineer",
-            "url": "https://hh.ru/vacancy/106954977",
-            "requirement": "Желателен опыт написания автотестов (UI/API) на <highlighttext>python</highlighttext>. Владение английским языком на уровне B2 и готовность немного поговорить с...",
-            "responsibility": "Функциональное тестирование frontend/backend (50/50). Разработка и написание тестовой/проектной документации. Заведение баг репортов.",
+            "name": "Разработчик",
+            "url": "https://hh",
+            "requirement": "требования",
+            "responsibility": "обязанности",
             "salary": 0
         },
         {
-            "name": "Junior Python разработчик",
-            "url": "https://hh.ru/vacancy/106571399",
-            "requirement": "Базовые знания <highlighttext>Python</highlighttext>. Понимание ООП. Опыт работы с фреймворками (например, Django, Flask). Знание SQL. Базовые знания HTML, CSS, JavaScript. ",
-            "responsibility": "Разработка и поддержка backend части приложения на <highlighttext>Python</highlighttext>. Работа с базами данных (например, PostgreSQL, MongoDB). Написание чистого, эффективного и хорошо...",
-            "salary": 125000
+            "name": "Разработчик2",
+            "url": "https://hh2",
+            "requirement": "требования2",
+            "responsibility": "обязанности2",
+            "salary": 120000
         }]
 
     return vacs
@@ -26,6 +26,8 @@ def vacancies_dict():
 @pytest.fixture
 def vacancies_objects():
     vacs = [Vacancy("Разработчик", "https://hh", "требования", "обязанности", 100000),
-            Vacancy("Разработчик1", "https://hh", "требования 1", "обязанности 1", 0)]
+            Vacancy("Разработчик1", "https://hh1", "требования 1", "обязанности 1", 0),
+            Vacancy("Разработчик2", "https://hh2", "требования 2", "обязанности 2", 50000),
+            Vacancy("Разработчик3", "https://hh3", "требования 3", "обязанности 3", 10000)]
 
     return vacs
